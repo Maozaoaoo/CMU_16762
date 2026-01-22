0ca8ec4004876d2840d918fc56b582bb141302e3
Maozaoaoo <haoliu0717@gmail.com>
2026-01-22 16:50:31 -0500

    lab1

--- Lab1/stretch_api.py
+++ Lab1/stretch_api.py
@@ -7,58 +7,53 @@ robot.startup()
 
 # Move the arm and gripper back to its stow position
 robot.stow()
-time.sleep(2)
-
+robot.wait_command()
 # Extend the arm and lift all the way out at the same time 
 robot.arm.move_to(0.5)
 robot.lift.move_to(1.1)
 
 robot.push_command()
-time.sleep(2)
+robot.wait_command()
 
 # Move the wrist joints
 robot.end_of_arm.move_to('wrist_yaw', np.radians(10))
 robot.push_command()
-time.sleep(2)
+robot.wait_command()
 
 robot.end_of_arm.move_to('wrist_pitch', np.radians(10))
 robot.push_command()
-time.sleep(2)
+robot.wait_command()
 
 robot.end_of_arm.move_to('wrist_roll', np.radians(10))
 robot.push_command()
-time.sleep(2)
+robot.wait_command()
 
 # Open the gripper and close it
 robot.end_of_arm.move_to('stretch_gripper', 50)
 robot.push_command()
-time.sleep(2)
+robot.wait_command()
 robot.end_of_arm.move_to('stretch_gripper', -20)
 robot.push_command()
-time.sleep(2)
-
-# robot.end_of_arm.move_to('stretch_gripper', -20)
-# robot.push_command()
-# time.sleep(2)
+robot.wait_command()
 
 # Rotate RealSense Camera
 robot.head.move_by('head_pan', np.radians(45))
 robot.head.move_by('head_tilt', np.radians(45))
 robot.push_command()
-time.sleep(2)
+robot.wait_command()
 
 robot.stow()
-time.sleep(2)
+robot.wait_command()
 
 # Move the robot
 robot.base.translate_by(0.5)
 robot.push_command()
-time.sleep(5)
+robot.wait_command()
 robot.base.rotate_by(np.deg2rad(180))
 robot.push_command()
-time.sleep(5)
+robot.wait_command()
 robot.base.translate_by(0.5)
 robot.push_command()
-time.sleep(5)
+robot.wait_command()
 
 robot.stop()
