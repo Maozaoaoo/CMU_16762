1b5d95cf758d1c2f1fe7d1de3b213e7f4e7d67e1
Maozaoaoo <haoliu0717@gmail.com>
2026-01-28 16:58:00 -0500

    lab1 part5

---
+++ Lab1/ros2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import time
+import rclpy
+import numpy as np
+import hello_helpers.hello_misc as hm
+
+def main():
+    rclpy.init()
+    node = hm.HelloNode()
+    node.start()
+
+    print("Stowing robot...")
+    node.stow_the_robot()
+
+    print("Extending arm and lift...")
+    node.move_to_pose({
+        'joint_arm': 0.5,
+        'joint_lift': 1.1
+    }, blocking=True)
+
+    print("Moving wrist joints...")
+    node.move_to_pose({'joint_wrist_yaw': 1.0}, blocking=True)
+    node.move_to_pose({'joint_wrist_pitch': -0.5}, blocking=True)
+    node.move_to_pose({'joint_wrist_roll': 1.0}, blocking=True)
+
+    print("Opening gripper...")
+    node.move_to_pose({'joint_gripper_finger_left': 0.1}, blocking=True)
+    time.sleep(1)
+
+    print("Closing gripper...")
+    node.move_to_pose({'joint_gripper_finger_left': 0.0}, blocking=True)
+
+    print("Moving head...")
+    node.move_to_pose({
+        'joint_head_pan': 1.0,
+        'joint_head_tilt': -0.5
+    }, blocking=True)
+
+    print("Stowing again...")
+    node.stow_the_robot()
+    time.sleep(3)
+
+    print("Driving base...")
+    node.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
+    node.move_to_pose({'rotate_mobile_base': np.pi}, blocking=True)
+    node.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
+
+    node.destroy_node()
+    rclpy.shutdown()
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
